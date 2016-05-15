854c75a025d05e3c71d8664c304a8c5bf4ccb472
Edwin <edwinvrooij@gmail.com>
2016-05-15 21:41:38 +0200

    created first class

--- tictactoe.py
+++ tictactoe.py
@@ -1,52 +1,19 @@
 #!/usr/bin/env
-
-# Declare constants
-amountOfColumns = 3
-amountOfRows = 3
+from datetime import datetime
 
 
-def write_board():
+class Game:
     """
-    Writes the Tic-Tac-Toe board
-    :return:
+    The Tic-Tac-Toe game itself. Also known as the board.
     """
-    print_top_row()
-    for i in range(amountOfColumns):
-        print_row(True)
+    winner = None
+    createdAt = None
+    endedAt = None
 
-def print_top_row():
+    def __init__(self):
+        global createdAt
+        createdAt = datetime.now()
+        print(createdAt)
 
+game = Game()
 
-def print_row(is_top_row=False, is_middle_row=False, is_bottom_row=False):
-    """
-    Prints a single row on the board
-    :param is_top_row: boolean, if true we'll write the top row
-    :param is_middle_row: boolean, if true we'll write a middle row
-    :param is_bottom_row: boolean, if true we'll write the bottom row
-    :return:
-    """
-    if is_top_row:
-        pass
-    if is_middle_row:
-        pass
-    if is_bottom_row:
-        pass
-
-
-def start_game():
-    """
-    Starts the game
-    :return:
-    """
-    print('Starting game...')
-    end_game()
-
-
-def end_game():
-    """
-    Ends the game
-    :return:
-    """
-    print('...end of game.')
-
-start_game()
