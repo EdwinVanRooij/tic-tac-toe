7b4404dfc2575a57cd39998221fab2b6e22625b2
Edwin <edwinvrooij@gmail.com>
2016-05-15 21:53:36 +0200

    implemented all classes

--- tictactoe.py
+++ tictactoe.py
@@ -11,9 +11,51 @@ class Game:
     endedAt = None
 
     def __init__(self):
-        global createdAt
-        createdAt = datetime.now()
-        print(createdAt)
+        self.createdAt = datetime.now()
+
+
+class Player:
+    """
+    A player participates in a game. A game has exactly two players.
+    """
+    turns = 5
+    turnHistory = []
+    name = 'Unset name in \'Player\' class'
+
+    def create_turn(self):
+        self.turnHistory.append(Turn())
+
+
+class Turn:
+    """
+    A turn in a game. A turn is created and executed by a player.
+    A turn contains meta data about the turn, like when the player created and executed the turn.
+    """
+    location = None
+    createdAt = None
+    executedAt = None
+
+    def __init__(self):
+        self.createdAt = datetime.now()
+
+    def execute_turn(self, x, y):
+        self.location = Location(x, y)
+
+    def get_time_taken(self):
+        return (self.createdAt-self.executedAt).total_seconds()
+
+
+class Location:
+    """
+    The location of the X or O placed by the player.
+    """
+    x = None
+    y = None
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 
 game = Game()
-
+print(game.createdAt)
