c714cd06ca280d08563a1044b08723fd2aef936c
Edwin <edwinvrooij@gmail.com>
2016-05-16 18:07:18 +0200

    completed testing template, moving it to the code snippets folder now

--- test_template.py
+++ test_template.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env
 import unittest
+import mymodule
 
 
 class TestNumbers(unittest.TestCase):
@@ -11,6 +12,7 @@ class TestNumbers(unittest.TestCase):
     numberTwo = None
     # Number three will be created & instantiated by the setup
     numberTen = None
+    calc = None
 
     def setUp(self):
         """
@@ -21,25 +23,33 @@ class TestNumbers(unittest.TestCase):
         self.numberTwo = 2
         self.numberThree = 3
         self.numberTen = 10
+        self.calc = mymodule.Calculator()
 
     def test_cumulative(self):
         """
-        Tests the correctness of variable instantiation by adding them up.
+        Test the add methods of my class 'Calculator' in module 'mymodule.py'.
+        Also tests the correctness of variable instantiation by adding them up.
         :return:
         """
-        expected = 10
-        # Count up to ten using the declared variables
-        actual = self.numberOne + self.numberOne + self.numberTwo + self.numberThree + self.numberThree
-        self.assertEqual(actual, expected)
+        # Test single addition
+        expected = 5
+        actual = mymodule.Calculator.add(self.numberTwo, self.numberThree)
+        self.assertEqual(actual, expected, 'single addition failed')
+
+        # Test multiple additions
+        expected = 16
+        actual = mymodule.Calculator.add_multiple_numbers(self.numberOne, self.numberTwo, self.numberThree, self.numberTen)
+        self.assertEqual(actual, expected, 'multiple additions failed')
 
     def test_division(self):
         """
         Tests the correctness of variable instantiation by subtracting with them.
         :return:
         """
-        expected = 1
-        actual = self.numberTen - self.numberThree - self.numberThree - self.numberOne - self.numberOne
-
+        expected = 5
+        # Below var should equal 5
+        var_to_subtract = mymodule.Calculator.add(self.numberThree, self.numberTwo)
+        actual = mymodule.Calculator.subtract(self.numberTen, var_to_subtract)
         self.assertEqual(actual, expected)
 
 
