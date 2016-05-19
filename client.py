3e6da21750634888677fa0fccd0a6db710d9b385
Edwin <edwinvrooij@gmail.com>
2016-05-19 21:28:35 +0200

    worked a bit on the board @ client

--- client.py
+++ client.py
@@ -2,15 +2,34 @@
 
 from src.tictactoe import *
 
+
+def print_board():
+    """
+    Prints the game board
+    :return:
+    """
+    print_line('-', 10)
+
+
+def print_line(string, times):
+    """
+    Prints a flexible line
+    :param string: the string to repeat
+    :param times: amount of times to repeat
+    :return:
+    """
+    string_list = []
+    for i in range(times):
+        string_list.append(string)
+    print(*string_list, end='\n')
+
+
 game = Game()
 
 bob_name = 'Bob'
 alice_name = 'Alice'
 
-bob = Player(bob_name)
-alice = Player(alice_name)
+game.add_player(bob_name)
+game.add_player(alice_name)
 
-game.add_player(bob)
-game.add_player(alice)
-
-
+print_board()
